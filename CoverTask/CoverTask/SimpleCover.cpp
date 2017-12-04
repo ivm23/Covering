651cf4fd76ede778053d00598d3b1503aa4d40cc
ivm23 <marina200964@yandex.ru>
2017-12-04 15:21:18 +0300

    field covered by more than two agents

--- CoverTask/CoverTask/SimpleCover.cpp
+++ CoverTask/CoverTask/SimpleCover.cpp
@@ -9,6 +9,7 @@ const int WHITE = 15;
 const int RED = 4;
 const int BACKGROUND = 0 << 4;
 const int NUMBER_OF_COLOR = 14;
+const int NONEPLAN = 0;
 
 ifstream in("map.txt");
 ofstream out("map.txt");
@@ -16,6 +17,8 @@ ofstream out("map.txt");
 int width = 0, height = 0;
 int countOfAgents = 0;
 long long countOfIteration = 0;
+vector<vector<int>> planMap;
+
 
 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
@@ -24,13 +27,14 @@ class Agent {
 private:
 	int color;
 	int x, y;
+	int planX = NONEPLAN, planY = NONEPLAN;
 public:
 	Agent(int c, int coordX, int coordY) : color(c), x(coordX), y(coordY) {}
 
 	~Agent() {};
 
-	int getColor() { return color;  }
-	pair<int, int> getCoordinates() { return make_pair(x,y); }
+	int getColor() { return color; }
+	pair<int, int> getCoordinates() { return make_pair(x, y); }
 
 	void printAgent() {
 		SetConsoleTextAttribute(hConsole, (WORD)(BACKGROUND | color % NUMBER_OF_COLOR + 1));
@@ -38,44 +42,57 @@ public:
 		SetConsoleTextAttribute(hConsole, (WORD)(BACKGROUND | WHITE));
 	}
 
-	pair<int, int> const getNewCoordinates(vector<vector<int>> & map) {
-		if (x + 1 < height && map[x + 1][y] == 0) {
-			x = x + 1; 
-			return make_pair(x, y);
+	void getNewCoordinates(vector<vector<int>> & planMap) {
+		if (planX == 0 && planY == 0) {
+			int step = 1;
+			bool cellSelected = false;
+			while (!cellSelected && (x + step < height || x - step >= 0 || y + step < width || y - step >= 0)) {
+				for (int ix = 0; ix <= step; ++ix) {
+					for (int iy = step; iy >= 0; --iy) {
+						if (!cellSelected && x + ix < height && y + iy < width && planMap[x + ix][y + iy] == 0) {
+							planMap[x + ix][y + iy] = color;
+							planX = x + ix;
+							planY = y + iy;
+							cellSelected = true;
+						}
+						if (!cellSelected && x - ix >= 0 && y + iy < width && planMap[x - ix][y + iy] == 0) {
+							planMap[x - ix][y + iy] = color;
+							planX = x - ix;
+							planY = y + iy;
+							cellSelected = true;
+						}
+						if (!cellSelected && x + ix < height && y - iy >= 0 && planMap[x + ix][y - iy] == 0) {
+							planMap[x + ix][y - iy] = color;
+							planX = x + ix;
+							planY = y - iy;
+							cellSelected = true;
+						}
+						if (!cellSelected && x - ix >= 0 && y - iy >= 0 && planMap[x - ix][y - iy] == 0) {
+							planMap[x - ix][y - iy] = color;
+							planX = x - ix;
+							planY = y - iy;
+							cellSelected = true;
+						}
+					}
+				}
+				++step;
+			}
 		}
 
-		if (x - 1 >= 0 && map[x - 1][y] == 0) {
-			x = x - 1;
-			return make_pair(x, y);
-		}
-		if (y + 1 < width && map[x][y + 1] == 0) {
-			y = y + 1;
-			return make_pair(x, y);
-		}
-		if (y - 1 >= 0 && map[x][y - 1] == 0) {
-			y = y - 1;
-			return make_pair(x, y);
-		}
-
-		// если нет свободных
-		if (x + 1 < height) {
-			x = x + 1;
-			return make_pair(x, y);
-		}
-
-		if (x - 1 >= 0) {
-			x = x - 1;
-			return make_pair(x, y);
-		}
-		if (y + 1 < width) {
-			y = y + 1;
-			return make_pair(x, y);
-		}
-		if (y - 1 >= 0) {
-			y = y - 1;
-			return make_pair(x, y);
-		}
 	}
+
+	pair<int, int> makeStep() {
+		if (x == planX || y == planY) {
+			y = planY;
+			x = planX;
+			planX = planY = 0;
+		}
+		else
+			x = planX;
+
+		return make_pair(x, y);
+	}
+
 };
 
 void makeAgents(vector<Agent> & agents) {
@@ -92,10 +109,13 @@ void const printAgents(vector<Agent> & agents) {
 
 void makeMap(vector<vector<int>> & map) {
 	map.resize(height);
+	planMap.resize(height);
 
 	for (int i = 0; i < height; ++i) {
-		for (int j = 0; j < width; ++j)
+		for (int j = 0; j < width; ++j) {
 			map[i].push_back(0);
+			planMap[i].push_back(0);
+		}
 	}
 }
 
@@ -105,10 +125,11 @@ void const printMap(vector<vector<int>> & map) {
 			if (*w != 0) {
 				SetConsoleTextAttribute(hConsole, (WORD)(BACKGROUND | (*w) % NUMBER_OF_COLOR + 1));
 			}
-		//	printf("%1.0f ", w);
-			cout << *w;
-			SetConsoleTextAttribute(hConsole, (WORD)(BACKGROUND | WHITE));
 			
+				printf("%2.0d", *w);
+			//cout << *w;
+			SetConsoleTextAttribute(hConsole, (WORD)(BACKGROUND | WHITE));
+
 		}
 		cout << endl;
 	}
@@ -138,7 +159,7 @@ int main() {
 
 	vector<Agent> agents;
 	vector<vector<int>> map;
-	
+
 	makeAgents(agents);
 	printAgents(agents);
 
@@ -149,20 +170,24 @@ int main() {
 
 	countOfIteration = 1;
 	--countOfCells;
-	
+
 	// пока не все покрыли
 	while (countOfCells != 0) {
 		for (auto agent = agents.begin(); agent != agents.end(); ++agent) {
-			auto newCoordinates = (*agent).getNewCoordinates(map);
 			++countOfIteration;
+
+			(*agent).getNewCoordinates(planMap);
+
+			auto newCoordinates = (*agent).makeStep();
 			if (map[newCoordinates.first][newCoordinates.second] == 0)
 				map[newCoordinates.first][newCoordinates.second] = (*agent).getColor(), --countOfCells;
 			printMap(map);
-			Sleep(100);
+			//	Sleep(100);
 		}
 	}
 
-	cout << countOfIteration << endl;
+	cout << "Number of iterations: " << countOfIteration << endl;
+	cout << "Map after covering: " << endl;
 	printMap(map);
 	system("Pause");
 	return 0;
